a39b0bbcd88abc0ee7832b0be252748902c12309
Sergey Kazarinov <kazarinov092@gmail.com>
2022-11-08 17:15:50 +0500

    feat(errors): add errors for the user

--- models/user.js
+++ models/user.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
+const AuthorizationError = require('../errors/AuthorizationError');
+const { LOGIN_ERROR_MESSAGE } = require('../utils/constants');
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -29,13 +31,13 @@ userSchema.statics.findUserByCredentials = function (email, password) {
   return this.findOne({ email }).select('+password')
     .then((user) => {
       if (!user) {
-        return Promise.reject(new Error());
+        return Promise.reject(new AuthorizationError(LOGIN_ERROR_MESSAGE));
       }
 
       return bcrypt.compare(password, user.password)
         .then((matched) => {
           if (!matched) {
-            return Promise.reject(new Error());
+            return Promise.reject(new AuthorizationError(LOGIN_ERROR_MESSAGE));
           }
 
           return user;
