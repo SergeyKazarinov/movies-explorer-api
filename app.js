fefe9df3f2b0ba162f7635a2be62a3967612f6f0
Sergey Kazarinov <kazarinov092@gmail.com>
2022-11-08 22:50:11 +0500

    feat(logger): add request.log and error.log

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const router = require('./routes');
 const err = require('./middlewares/error');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 
@@ -15,7 +16,11 @@ mongoose.connect('mongodb://localhost:27017/bitfilmsdb');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(requestLogger);
+
 app.use(router);
+
+app.use(errorLogger);
 
 app.use(errors());
 app.use(err);
