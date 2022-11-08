39a531063578bf5a23d68bc623ba30e593528978
Sergey Kazarinov <kazarinov092@gmail.com>
2022-11-08 23:36:51 +0500

    feat(validation): add celebrate valid for the user

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,9 @@ const user = require('express').Router();
 const {
   getUser, patchUser,
 } = require('../controllers/users');
+const patchUserValidation = require('../validation/patchUserValidation');
 
 user.get('/users/me', getUser);
-user.patch('/users/me', patchUser);
+user.patch('/users/me', patchUserValidation, patchUser);
 
 module.exports = user;
