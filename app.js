f2c45c4d1ae96c8f9d034790fc9fe29bc0aacfe8
Sergey Kazarinov <kazarinov092@gmail.com>
2022-11-09 16:54:56 +0500

    feat(limiter): add rate limiter

--- app.js
+++ app.js
@@ -2,17 +2,21 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const bodyParser = require('body-parser');
+const helmet = require('helmet');
 const express = require('express');
 const router = require('./routes');
 const err = require('./middlewares/error');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
+const limiter = require('./middlewares/rateLimit/limiter');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/bitfilmsdb');
+mongoose.connect('mongodb://localhost:27017/moviesdb');
 
+app.use(helmet());
+app.use(limiter);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
